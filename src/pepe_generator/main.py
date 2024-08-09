0640dbb1f3995072dd235bc7499a0d4b2ca0a1f0
horken7 <horken7@gmail.com>
2024-08-09 13:48:54 +0200

    manually download dataset to avoid creating new folders

--- src/pepe_generator/main.py
+++ src/pepe_generator/main.py
@@ -1,5 +1,6 @@
 import os
 import random
+from zipfile import ZipFile
 
 import requests
 import uvicorn
@@ -13,11 +14,38 @@ if (
     and os.environ.get("KAGGLE_USERNAME")
     and os.environ.get("KAGGLE_KEY")
 ):
-    import kaggle
-
-    kaggle.api.dataset_download_files(
-        "tornikeonoprishvili/pepe-memes-dataaset", unzip=True, path="data/pepe"
+    # URL for the dataset download
+    URL = (
+        "https://www.kaggle.com/api/v1/datasets/download/tornikeonoprishvili/"
+        "pepe-memes-dataaset?datasetVersionNumber=1"
     )
+
+    # Destination folder
+    DEST_FOLDER = "data"
+
+    # Path to save the downloaded file
+    zip_path = os.path.join(DEST_FOLDER, "pepe-memes-dataset.zip")
+
+    # Download the dataset
+    response = requests.get(
+        URL,
+        auth=(os.environ.get("KAGGLE_USERNAME"), os.environ.get("KAGGLE_KEY")),
+        timeout=100,
+    )
+    response.raise_for_status()  # Check if the request was successful
+
+    # Save the downloaded zip file
+    with open(zip_path, "wb") as file:
+        file.write(response.content)
+
+    print(f"Downloaded dataset to {zip_path}")
+
+    # Unzip the file
+    with ZipFile(zip_path, "r") as zip_ref:
+        zip_ref.extractall(DEST_FOLDER)
+
+    print(f"Extracted files to {DEST_FOLDER}")
+
 
 default_image_response = requests.get(
     "https://upload.wikimedia.org/wikipedia/en/6/63/Feels_good_man.jpg",
@@ -29,8 +57,10 @@ default_image_response = requests.get(
 async def return_pepe():
     if not os.environ.get("KAGGLE_ENABLE"):
         return Response(default_image_response.content, media_type="image/jpeg")
-    random_image = random.choice([f for f in os.listdir("data/pepe/data") if not f.endswith('.txt')])
-    image_path = os.path.join("data/pepe/data", random_image)
+    random_image = random.choice(
+        [f for f in os.listdir("data/data") if not f.endswith(".txt")]
+    )
+    image_path = os.path.join("data/data", random_image)
     return FileResponse(image_path)
 
 
